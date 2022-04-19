ccef63e6cd6e96c717f8b2882056a5e42f13d4db
Jackieok01 <ocampojacqueline83@gmail.com>
2022-04-19 12:15:32 -0300

    calculadora

---
+++ funcionalidades.js
@@ -0,0 +1,11 @@
+ var num1
+ var num2
+ var calculo
+ 
+ // agregamos una funcion
+ Function ingreso (){
+ var resultado= Document.getElementById("resultado")
+ //realizar el mismo procedimiento del 1 al 9, suma , resta
+ // multi, borrar , etc
+ 
+ }
